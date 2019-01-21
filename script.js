da90d8a4b294aa045e52d8eaf01bd487747a47b5
Mishiiii <43751802+Mishiiii@users.noreply.github.com>
2019-01-21 04:57:00 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#LUMOS").click(function() {
+    $("#LIGHT").fadeToggle();
+});
+
+$("#RIDDIKULUS").click(function() {
+    $("#SHAPESHIFTER").toggle();
+});
+
+$("#ALOHOMORA").click(function() {
+    $("#DOOR").slideToggle();
+});
